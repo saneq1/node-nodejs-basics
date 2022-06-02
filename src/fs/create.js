917eada1cbc72418901dfeb17084a6901e9473cb
saneq1 <thesaneq@gmail.com>
2022-06-02 06:52:24 +0300

    added:first task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import fs from 'fs'
+import path from "path"
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+
 export const create = async () => {
-    // Write your code here 
+    fs.access(`${path.dirname(__filename)}/files/fresh.txt`, function(access){
+        if (access) {
+            fs.writeFile(`${path.dirname(__filename)}/files/fresh.txt`, "I am fresh and young", function(error){
+                if(error) throw error; 
+                console.log('Create file fresh.txt');
+            });
+        } else {
+            throw "FS operation failed"
+        }
+    })
 };
+
+create()

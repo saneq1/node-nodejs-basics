f34d058c1ed4ee0b5c09a8671bbb9ddc7a1b4db0
saneq1 <sanyq90@mail.ru>
2022-06-02 12:06:43 +0300

    added:second task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,31 @@
+import fs from 'fs'
+import path from "path"
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+
+
 export const copy = async () => {
-    // Write your code here 
+    fs.access(`${path.dirname(__filename)}/files`, function(no_access){ // проверка на наличие папки
+        if (no_access) {
+            throw "FS operation failed - no field files"
+        } else {
+         fs.access(`${path.dirname(__filename)}/files_copy`, function(no_files_copy_access){ // проверка на наличие папки
+                if (no_files_copy_access) {
+                    // here
+                    fs.mkdir(`${path.dirname(__filename)}/files_copy`, err => { //создание папки
+                        if(err) throw "FS operation failed - no create field"; 
+                        else {
+                            fs.cpSync(`${path.dirname(__filename)}/files`, `${path.dirname(__filename)}/files_copy`, {recursive: true}) //копирование содержимого папки в папку
+                        }
+                     });
+                } else {
+                    throw "FS operation failed - field files_copy exist"
+                }
+            })
+        }
+    })
 };
+
+copy()
+
--- src/fs/create.js
+++ src/fs/create.js
@@ -5,9 +5,9 @@ const __filename = fileURLToPath(import.meta.url);
 
 
 export const create = async () => {
-    fs.access(`${path.dirname(__filename)}/files/fresh.txt`, function(access){
+    fs.access(`${path.dirname(__filename)}/files/fresh.txt`, function(access){ // проверка на наличие файла
         if (access) {
-            fs.writeFile(`${path.dirname(__filename)}/files/fresh.txt`, "I am fresh and young", function(error){
+            fs.writeFile(`${path.dirname(__filename)}/files/fresh.txt`, "I am fresh and young", function(error){ // создание файла с текстом
                 if(error) throw error; 
                 console.log('Create file fresh.txt');
             });

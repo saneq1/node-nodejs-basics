04c1dc1a444156346faeaf17a224b670c3f3df0c
saneq1 <sanyq90@mail.ru>
2022-06-03 10:44:02 +0300

    added:10 task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,15 @@
+import crypto from "crypto"
+import fs from "fs"
+import path from "path"
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const fileBuffer = fs.readFileSync(`${path.dirname(__filename)}/files/fileToCalculateHashFor.txt`);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
 };
+
+calculateHash()
